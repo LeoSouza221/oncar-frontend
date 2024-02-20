8d1b92059dacd62ed846107af6964f818be05e67
LeoSouza221 <leosouza221@gmail.com>
2024-02-20 11:33:59 -0300

    feat: adjust edit modal component

--- src/components/layout/EditCarModal.vue
+++ src/components/layout/EditCarModal.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue';
+import { computed, ref, reactive } from 'vue';
 import type { PropType } from 'vue';
 import type { Car } from '@/types/car';
 import { useCarsStore } from '@/stores/manageCars';
 import InputCompnent from '@/components/ui/InputComponent.vue';
 import ModalComponent from '@/components/layout/ModalComponent.vue';
-
-const inputError = ref<boolean>(true);
-const inputErrorMessage = ref<string>('');
-const carStore = useCarsStore();
 
 const props = defineProps({
   modalValue: {
@@ -20,10 +16,25 @@ const props = defineProps({
     required: true,
   },
 });
-
 const emit = defineEmits(['update:modalValue', 'update:isModalOpen']);
 
-const todoItem = computed({
+const carStore = useCarsStore();
+const inputsErrors = reactive({
+  model: {
+    hasError: false,
+    message: '',
+  },
+  brand: {
+    hasError: false,
+    message: '',
+  },
+  color: {
+    hasError: false,
+    message: '',
+  },
+});
+
+const car = computed({
   get() {
     return props.modalValue;
   },
@@ -41,27 +52,77 @@ const openModal = computed({
   },
 });
 
-function changeTodoItem() {
-  const isOnlySpaces = todoItem.value.item.replace(/\s/g, '').length;
+function isValidModel() {
+  const isOnlySpaces = car.value.model.replace(/\s/g, '').length;
 
-  if (!todoItem.value.item.length) {
-    inputError.value = true;
-    inputErrorMessage.value = 'Digite pelo menos 1 caracter';
+  if (!car.value.model.length) {
+    inputsErrors.model.hasError = true;
+    inputsErrors.model.message = 'Digite pelo menos 1 caracter';
 
-    return;
+    return false;
+  } else if (!isOnlySpaces) {
+    inputsErrors.model.hasError = true;
+    inputsErrors.model.message = 'Itens não devem conter somente espaços';
+
+    return false;
   }
 
-  if (!isOnlySpaces) {
-    inputError.value = true;
-    inputErrorMessage.value = 'Itens não devem conter somente espaços';
+  return true;
+}
 
-    return;
+function isValidBrand() {
+  const isOnlySpaces = car.value.brand.replace(/\s/g, '').length;
+
+  if (!car.value.brand.length) {
+    inputsErrors.brand.hasError = true;
+    inputsErrors.brand.message = 'Digite pelo menos 1 caracter';
+
+    return false;
+  } else if (!isOnlySpaces) {
+    inputsErrors.brand.hasError = true;
+    inputsErrors.brand.message = 'Itens não devem conter somente espaços';
+
+    return false;
   }
 
-  carStore.changeCarItem(todoItem.value);
+  return true;
+}
 
+function isValidColor() {
+  const isOnlySpaces = car.value.color.replace(/\s/g, '').length;
+
+  if (!car.value.color.length) {
+    inputsErrors.color.hasError = true;
+    inputsErrors.color.message = 'Digite pelo menos 1 caracter';
+
+    return false;
+  } else if (!isOnlySpaces) {
+    inputsErrors.color.hasError = true;
+    inputsErrors.color.message = 'Itens não devem conter somente espaços';
+
+    return false;
+  }
+
+  return true;
+}
+
+function clearAndCloseModal() {
   openModal.value = false;
-  inputError.value = false;
+  inputsErrors.brand.hasError = false;
+  inputsErrors.color.hasError = false;
+  inputsErrors.model.hasError = false;
+}
+
+function validateAndSubmit(e: Event) {
+  e.preventDefault();
+
+  if (!isValidModel()) return;
+  if (!isValidBrand()) return;
+  if (!isValidColor()) return;
+
+  carStore.changeCarItem(car.value);
+
+  clearAndCloseModal();
 }
 </script>
 
@@ -69,30 +130,56 @@ function changeTodoItem() {
   <ModalComponent v-model="openModal">
     <div class="card w-[400px] dark:bg-slate-700 dark:text-white">
       <div>
-        <h3 class="text-2xl">Editar tarefa</h3>
+        <h3 class="text-2xl">Editar carro</h3>
       </div>
-      <div class="py-2">
-        <InputCompnent
-          v-model="todoItem.item"
-          :error="inputError"
-          :error-message="inputErrorMessage"
-          @update:update-model="changeTodoItem()"
-        />
-      </div>
+      <form @submit="validateAndSubmit">
+        <div class="pt-2">
+          <label>
+            Modelo
+            <InputCompnent
+              v-model="car.model"
+              :error="inputsErrors.model.hasError"
+              :error-message="inputsErrors.model.message"
+            />
+          </label>
+        </div>
 
-      <div class="pt-2 flex justify-between">
-        <button
-          class="bg-red-600 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
-          @click="openModal = false"
-          >Cancelar</button
-        >
+        <div class="grid grid-cols-2 gap-2">
+          <label>
+            Marca
+            <InputCompnent
+              v-model="car.brand"
+              :error="inputsErrors.brand.hasError"
+              :error-message="inputsErrors.brand.message"
+            />
+          </label>
+          <label>
+            Cor
+            <InputCompnent
+              v-model="car.color"
+              :error="inputsErrors.color.hasError"
+              :error-message="inputsErrors.color.message"
+              @update:update-model="validateAndSubmit"
+            />
+          </label>
+        </div>
 
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          @click="changeTodoItem"
-          >Confirmar</button
-        >
-      </div>
+        <div class="pt-2 flex justify-between">
+          <button
+            class="bg-red-600 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
+            @click="clearAndCloseModal"
+          >
+            Cancelar
+          </button>
+
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Confirmar
+          </button>
+        </div>
+      </form>
     </div>
   </ModalComponent>
 </template>
